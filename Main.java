5ae0ea9147a92273323faa128c4b7051e98c1835
MarinaChvek <52216049+MarinaChvek@users.noreply.github.com>
2019-06-27 11:52:30 +0300

    Задание 1.3

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner; 
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	   Scanner in = new Scanner(System.in);
+                   int n=in.nextInt();
+	   String str = "Вы ввели число: ";
+	   System.out.println(str + n);   
+    }
+}
