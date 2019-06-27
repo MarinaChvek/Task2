c041d14aa064f40228602e79858fc1d38f4f9a41
MarinaChvek <52216049+MarinaChvek@users.noreply.github.com>
2019-06-27 12:33:35 +0300

    Задание 1.8

---
+++ Main2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main2 {
+
+    public static void main(String[] args) {
+        Scanner a = new Scanner(System.in);
+        Scanner b = new Scanner(System.in);
+        Scanner c = new Scanner(System.in);
+        Scanner d = new Scanner(System.in);
+        System.out.println(a.nextLine() + " " + b.nextLine() + " " + c.nextLine() + " " + d.nextLine()  );
+
+    }
+}
