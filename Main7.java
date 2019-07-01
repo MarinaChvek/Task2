54808336bb6b562c8f2faf0612b7aaf1f145140e
MarinaChvek <52216049+MarinaChvek@users.noreply.github.com>
2019-07-01 16:40:12 +0300

    Задание 1.25

---
+++ Main7.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+    public class Main7 {
+
+        public static void main(String[] args) {
+              System.out.println("Введите сторону квадрата: ");
+              Scanner in = new Scanner(System.in);
+              double a = in.nextDouble();
+              System.out.println("Периметр квадрата равен = " + 4 * a);
+    }
+}
