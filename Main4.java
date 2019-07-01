553a1def432ff084d079a392eba7b53f72d94bdd
MarinaChvek <52216049+MarinaChvek@users.noreply.github.com>
2019-07-01 16:34:56 +0300

    Задание 1.12

---
+++ Main4.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main4 {
+
+    public static void main(String[] args) {
+    int a=5;
+    int b=10;
+    int c=7;
+    System.out.print (a+" "+b+"\n"+c+" см");
+ 
+
+    int m=100;
+    Scanner in=new Scanner (System.in);
+    System.out.print ("\n" +m+" ");
+    int t=in.nextInt();
+    int n=1949;
+    System.out.print(n+" ");
+    int z=in.nextInt();
+
+
+int x = in.nextInt();
+int y = in.nextInt();
+
+int s=5;
+ Scanner f=new Scanner (System.in);
+System.out.printf ("%d %d \n",f.nextInt(),s);
+    in.close();
+    }
+}
